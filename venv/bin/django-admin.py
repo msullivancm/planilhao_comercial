960c5d139f41b4642ca88eae4becbaef7f4f4f7d
Marcus Sullivan <sullivan@pop-os.localdomain>
2020-08-05 13:16:00 -0300

    teste

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/dados/insync/Clientes/BDM/SOLUÇÕES/Planilhão-Comercial/planilhao_comercial/venv/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
